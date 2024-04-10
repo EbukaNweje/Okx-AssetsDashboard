a5e5d50cacb14ec872b75ba5718da326129aba68
eflexTech <eflexmedia1@gmail.com>
2024-04-09 17:21:50 -0700

    F

---
+++ src/Pages/Deposit/Payment.jsx
@@ -0,0 +1,119 @@
+import {FaCopy} from "react-icons/fa";
+import "./Payment.css";
+import {MdDownloading} from "react-icons/md";
+import { useParams } from "react-router";
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { updateDepositData } from "../../Components/store/FeaturesSlice";
+
+const Payment = () => {
+    const {paymentname, id} = useParams()
+    const [pay, setpay] = useState(false)
+    let amount = JSON.parse(localStorage.getItem("amount"))
+    const [isButtonDisabled, setButtonDisabled] = useState(false);
+
+
+
+    const nav = useNavigate()
+    console.log(amount);
+
+    const depositDatas = 
+        {
+            amount: amount,
+            paymentMode: paymentname,
+            status: "success",
+            dateCreated: new Date().toDateString(),
+        }
+    const dispatch = useDispatch()
+    // console.log(depositData)
+
+    const [state, setState] = useState({
+        value: `${paymentname === "BITCOINP PAYMENT"? ("bc1qdg0mvuud5qq3933r7y6v8s2x20fgq0ldsfc5uy"): paymentname === "ETHEREUM PAYMENT"? ("0xf0d3E618F77642c32Cded583Eb000e4cA8f76FbB"): paymentname ==="USDT PAYMENT"? ("0xf0d3E618F77642c32Cded583Eb000e4cA8f76FbB"): "Chosse a Payment Method"}`,
+        copied: false,
+      });
+
+      const url = `https://okx-assetsbackend.onrender.com/api/sendpayment/${id}`
+      
+      const payNow = ()=> {
+        setButtonDisabled(true)
+        axios.post(url, {Amount:amount})
+        .then(res => {
+          console.log(res)
+          setpay(true)
+
+        }).catch((err)=>{
+          console.log(err)
+        })
+      }
+
+    return (
+        <>
+            <div className="DepPaymentBody">
+                <h1>Make Payment</h1>
+                <div className="DepPaymentContent">
+                    <div className="DepPaymentContentWrap">
+                        <div className="DepPaymentContentA">
+                            <div>Your payment method</div>
+                            <p>
+                                {paymentname}{" "}
+                                <span>
+                                    <MdDownloading />
+                                </span>
+                            </p>
+                        </div>
+                        <p className="DepPaymentContentB">
+                            You are to make payment of ${amount} using your selected
+                            payment method.
+                        </p>
+                        <div className="DepPaymentContentC">
+                            <p>{paymentname} Address:</p>
+                            <div className="DepPaymentContentCTopReferUsDivBox">
+                                <input
+                                    type="text"
+                                    value={state.value}
+                                    readOnly
+                                />
+                                <CopyToClipboard
+                                 text={state.value}
+                                 onCopy={() => setState({ copied: true })}
+                                 >
+                                <div className="DepPaymentContentCTopReferUsDivBoxCopy">
+                                    <FaCopy />
+                                </div>
+                                 </CopyToClipboard>
+                            </div>
+                            <h5>Network Type:<span>{paymentname=== "BITCOINP PAYMENT"? "BTC" : paymentname=== "ETHEREUM PAYMENT"? "ETH" :  paymentname === "USDT PAYMENT" ? "USDT" : paymentname=== "ETHEREUM PAYMENT"? "ETH" :  paymentname === "BNB PAYMENT" ? "BNB": null}</span></h5>
+                        </div>
+                        <div className="DepPaymentContentD">
+                            <p>Upload Payment proof after payment.</p>
+                            <div className="DepPaymentContentDUpload">
+                                <input type="file" />
+                            </div>
+                            <button onClick={payNow}
+                            disabled={isButtonDisabled}
+                            >
+                                {
+                                    isButtonDisabled ?  "Submitting..." : 'Submit Payment'
+                                }
+                                </button>
+                        </div>
+                    </div>
+                </div>
+
+          {pay?
+          <div className='SuccessPaid'>
+                <div className='PayCon'>
+                    <h3>You have successfully made a deposit </h3>
+                    <button style={{width: "50%", height: "40px", background:"#0e4152", border:"none", color:"white", fontSize:"15px"}} onClick={()=>{setpay(false); nav(`/${id}`); dispatch(updateDepositData(depositDatas))}}>Ok</button>
+                </div>
+            </div>: 
+            null}
+            </div>
+        </>
+    );
+};
+
+export default Payment;
